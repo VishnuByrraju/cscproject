e075cbab367afce469c64ca032b71bfeb63f7bea
VishnuByrraju <byrrajuvishnuvardhan@gmail.com>
2025-04-03 08:04:42 +0530

    Updated Edit Profile

--- src/components/UserProfileComponent.jsx
+++ src/components/UserProfileComponent.jsx
@@ -92,15 +92,33 @@ const fetchUserProfile = async () => {
   const handleUpdateProfile = async () => {
     setLoading(true);
     try {
-      const response = await fetch(`${BASE_URL}/v1/edit-profile/${userMail}/${userRole}`, {
-        method: 'PUT',
+      const response2 = await fetch(`https://55cr1n59r3.execute-api.us-east-2.amazonaws.com/dev/v1/user/edit-profile`, {
+        method: 'POST',
         headers: {
           'Authorization': `Bearer ${accesstoken}`,
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
+          "body": {
+          email,
           first_name: userData.first_name,
           last_name: userData.last_name,
+          }
+        }),
+      });
+
+      const response = await fetch(`https://55cr1n59r3.execute-api.us-east-2.amazonaws.com/dev/v1/user/edit-profile`, {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${accesstoken}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          "body": {
+          email,
+          first_name: userData.first_name,
+          last_name: userData.last_name,
+          }
         }),
       });
 
@@ -209,7 +227,7 @@ const fetchUserProfile = async () => {
         <div className="container max-w-4xl mx-auto p-6 bg-white shadow-md rounded-md">
 
           <div className="flex flex-col lg:flex-row lg:space-x-8">
-            <div className="w-full lg:w-1/2 mb-8 lg:mb-0">
+            <div className="w-full lg:w-full mb-8 lg:mb-0">
               <h2 className="text-xl mb-4">My Profile</h2>
               {loading ? (
                 <LoadingScreenLarge />
@@ -256,58 +274,14 @@ const fetchUserProfile = async () => {
                 </>
               )}
             </div>
-  
-            <div className="w-full lg:w-1/2">
-              <h2 className="text-xl mb-4">Change Password</h2>
-              {passwordLoading ? (
-                <LoadingScreenLarge />
-              ) : (
-                <>
-                  <div className="mb-4">
-                    <label className="block text-gray-700 mb-1">New Password</label>
-                    <input
-                      type="password"
-                      name="newPassword"
-                      value={passwordData.newPassword}
-                      onChange={handlePasswordChange}
-                      className="w-full p-2 border rounded"
-                    />
-                  </div>
-                  <div className="mb-4">
-                    <label className="block text-gray-700 mb-1">Retype New Password</label>
-                    <input
-                      type="password"
-                      name="retypeNewPassword"
-                      value={passwordData.retypeNewPassword}
-                      onChange={handlePasswordChange}
-                      className="w-full p-2 border rounded"
-                    />
-                  </div>
-                  {passwordError && (
-                    <p className="text-red-500 mb-4">{passwordError}</p>
-                  )}
-                  <button
-                    onClick={handleUpdatePassword}
-                    className="px-4 py-2 text-white bg-primary rounded-lg hover:bg-red-800"
-                    disabled={passwordLoading}
-                  >
-                    Change Password
-                  </button>
-                </>
-              )}
+              
               {updateProfileSuccess && (
                 <PopupComponent content={updateProfileSuccess} contentType={"Update Profile Status"} redirectPath={""}/>
               )}
               {updateProfileError && (
                 <PopupComponent content={updateProfileError} contentType={"Update Profile Status"} redirectPath={""}/>
               )}
-              {updatePasswordSuccess && (
-                <PopupComponent content={updatePasswordSuccess} contentType={"Update Password Status"} redirectPath={""}/>
-              )}
-              {updatePasswordError && (
-                <PopupComponent content={updatePasswordError} contentType={"Update Password Status"} redirectPath={""}/>
-              )}
-            </div>
+              
           </div>
         </div>
       </div>
